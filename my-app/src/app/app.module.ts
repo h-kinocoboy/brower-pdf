40d8f9391d099b2857a68065b1363848a13953a8
kinocoboy <h.kinocoboy@gmail.com>
2022-08-24 11:29:54 +0900

    feat: add file element

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgxFileDropModule } from 'ngx-file-drop';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxFileDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]

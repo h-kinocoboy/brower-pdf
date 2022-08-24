c9bd71952b550a222de7b7688e547d180f878372
kinocoboy <h.kinocoboy@gmail.com>
2022-08-24 12:14:39 +0900

    feat: pdfを別画面に開く

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -19,25 +19,15 @@ export class AppComponent {
         const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
         fileEntry.file((file: File) => {
 
-          // Here you can access the real file
+
+          // ここでFile 形式に変換できればよい
           console.log(droppedFile.relativePath, file);
 
-          /**
-          // You could upload it like this:
-          const formData = new FormData()
-          formData.append('logo', file, relativePath)
 
-          // Headers
-          const headers = new HttpHeaders({
-            'security-token': 'mytoken'
-          })
-
-          this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
-          .subscribe(data => {
-            // Sanitized logo returned from backend
-          })
-          **/
-
+          // 動的に別タブを開く
+          const url = URL.createObjectURL(file)
+          console.log(url)
+          window.open(url, "_blank")
         });
       } else {
         // It was a directory (empty directories are added, otherwise only files)
